9568ef77aa29f7dfdde99c1b0bc8a832be14c295
Jhon Arley Castillo Vitovis <jhona_castillov@soy.sena.edu.co>
2023-12-03 16:16:16 -0500

    Se termina script js

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-
+function toggleMenu() {
+    const menu = document.querySelector(".memu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
 
